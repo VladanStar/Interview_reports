ef1ce85aafa3eef0ffd2a6b57a2688943dace9f3
Nikola Colovic <nikcolovic@gmail.com>
2021-09-20 09:25:25 +0200

    login makeup

--- interview-reports/src/app/entities/Login/Login.js
+++ interview-reports/src/app/entities/Login/Login.js
@@ -24,7 +24,7 @@ const Login = ({ submit, email, password, onChangeEmail, onChangePassword }) => 
           />
           </div>
           <div className="buttons">
-          <button className="single-button">Back to Home</button>
+          
           <button className="single-button" onClick={submit}>Log in</button>
           </div>
           </form>
